10f5df836967ea08cf5b95f4c64a80f211be13db
MOHD USMAN ALI <40804741+mohdusman63@users.noreply.github.com>
2020-12-08 14:25:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import UserForm from './Form/UserForm'
+
+function App() {
+
+    return (
+        <>
+        <UserForm/>
+
+        </>
+
+
+
+
+    );
+}
+
+export default App;
